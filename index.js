799303102ece15c6298c4a383025b23396ff5f9f
vedansh <me.vedanshwani@gmail.com>
2024-01-30 15:50:22 +0530

    calculating frames per row

--- index.js
+++ index.js
@@ -13,13 +13,36 @@ image.src = "./shadow_dog.png";
 let frameX = 0;
 let frameY = 0;
 let gameFrame = 0;
-const staggerAnimation = 7;
+const staggerFrames = 7;
+
+const animationStates = [
+  { name: "idle", frames: 7 },
+  { name: "jump", frames: 7 },
+];
+const spriteAnimations = [];
+
+animationStates.forEach((state, index) => {
+  const { name, frames } = state;
+  const location = [];
+
+  for (let i = 0; i < frames; i++) {
+    location.push({ x: i * spriteWidth, y: index * spriteHeight });
+  }
+
+  spriteAnimations.push({ [name]: location });
+});
+
+console.log(spriteAnimations);
 
 function animate() {
   ctx.clearRect(0, 0, CANVAS_WIDTH, CANVAS_HEIGHT);
+  const position = Math.floor(gameFrame / staggerFrames) % 6;
+
+  frameX = position * spriteWidth;
+
   ctx.drawImage(
     image,
-    frameX * spriteWidth,
+    frameX,
     frameY * spriteHeight,
     spriteWidth,
     spriteHeight,
