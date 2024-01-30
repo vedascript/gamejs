77a0724497b0ef25fb4fca67fb3e905784209844
vedansh <me.vedanshwani@gmail.com>
2024-01-30 18:59:39 +0530

    added extra animation states

--- index.js
+++ index.js
@@ -13,13 +13,22 @@ image.src = "./shadow_dog.png";
 let frameX = 0;
 let frameY = 0;
 let gameFrame = 0;
+let animationType = "ko";
+const spriteAnimations = [];
 const staggerFrames = 7;
 
 const animationStates = [
   { name: "idle", frames: 7 },
   { name: "jump", frames: 7 },
+  { name: "fall", frames: 7 },
+  { name: "run", frames: 9 },
+  { name: "dizzy", frames: 11 },
+  { name: "sit", frames: 5 },
+  { name: "roll", frames: 7 },
+  { name: "bite", frames: 7 },
+  { name: "ko", frames: 12 },
+  { name: "getHit", frames: 4 },
 ];
-const spriteAnimations = [];
 
 animationStates.forEach((state, index) => {
   const { name, frames } = state;
@@ -29,21 +38,25 @@ animationStates.forEach((state, index) => {
     location.push({ x: i * spriteWidth, y: index * spriteHeight });
   }
 
-  spriteAnimations.push({ [name]: location });
+  spriteAnimations[name] = location;
 });
 
 console.log(spriteAnimations);
 
 function animate() {
   ctx.clearRect(0, 0, CANVAS_WIDTH, CANVAS_HEIGHT);
-  const position = Math.floor(gameFrame / staggerFrames) % 6;
 
-  frameX = position * spriteWidth;
+  const totalFrames = spriteAnimations[animationType].length;
+  const position = Math.floor(gameFrame / staggerFrames) % totalFrames;
+
+  // frameX = position * spriteWidth;
+  const x = spriteAnimations[animationType][position].x;
+  const y = spriteAnimations[animationType][position].y;
 
   ctx.drawImage(
     image,
-    frameX,
-    frameY * spriteHeight,
+    x,
+    y,
     spriteWidth,
     spriteHeight,
     0,
