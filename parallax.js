8da7359e14ec7733cfc8e7f1b4dcb521e301b59a
vedansh <me.vedanshwani@gmail.com>
2024-01-31 09:59:17 +0530

    parallax

--- parallax.js
+++ parallax.js
@@ -1,7 +1,34 @@
 const canvas = document.querySelector("#parallax");
 const ctx = canvas.getContext("2d");
 
-canvas.width = 800;
-canvas.height = 700;
+const canvasWidth = (canvas.width = 800);
+const canvasHeight = (canvas.height = 700);
 
-let gameSpeed = 5;
+let gameSpeed = 15;
+
+const image4 = new Image();
+image4.src = "./backgroundLayers/layer-4.png";
+let x = 0;
+let x2 = 2400;
+
+function animate() {
+  ctx.clearRect(0, 0, canvasWidth, canvasHeight);
+  ctx.drawImage(image4, x, 0);
+  ctx.drawImage(image4, x2, 0);
+
+  if (x < -2400) {
+    x = 2400;
+  } else {
+    x -= gameSpeed;
+  }
+
+  if (x2 < -2400) {
+    x2 = 2400;
+  } else {
+    x2 -= gameSpeed;
+  }
+  console.log(x);
+  requestAnimationFrame(animate);
+}
+
+animate();
